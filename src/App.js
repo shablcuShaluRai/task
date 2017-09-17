4704b4aeade7e5dcbd215e33eb25f2446bb7c18b
shalurai <shaluraics22@gmail.com>
2017-09-18 02:02:59 +0900

    adding header

--- src/App.js
+++ src/App.js
@@ -10,33 +10,44 @@ class App extends Component {
      data : {}
   }
 
+  componentDidMount() {
+  DataApi
+  .getAllData()
+  .then(data => {
+    this.setState({data:data })
+    console.log(data);
+  })
+}
 
-    getData(){
-      DataApi
-      .getAllData()
-      .then(data => {
-        this.setState({data:data })
-      })
-
-    }
+    // getData(){
+    //   DataApi
+    //   .getAllData()
+    //   .then(data => {
+    //     this.setState({data:data})
+    //     console.log(data);
+    //   })
+    //
+    // }
 
   render() {
+
     return (
       <div className="App">
-      <h1> hello</h1>
 
+   <div className = "header">
+   <h1> {this.state.data.title} </h1>
+
+     </div>
         <div>
-        <p> {this.state.data.id}</p>
-        <p> {this.state.data.title} </p>
         <p> {this.state.data.applications_close_date}</p>
         <p> {this.state.data.earliest_start_date}</p>
         <p>{this.state.data.latest_end_date}</p>
         <p>{this.state.data.description}</p>
-
-        </div>
-
-      <button onClick={this.getData.bind(this)}>click</button>
       </div>
+
+    {/*  <button onClick={this.getData.bind(this)}>click</button> */}
+      </div>
+
     );
   }
 }
